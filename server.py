8b287315beb86530740189ceaa89d3327cb16477
npdev <sashko.dev@gmail.com>
2023-09-18 16:52:39 +0100

    basic personalization

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 import csv
 
 from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
 
 def write_to_csv(data):
     with open('database.csv', mode='a', newline='') as database:
@@ -17,7 +21,8 @@ def write_to_csv(data):
         subject = data['subject']
         message = data['message']
         csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
+        csv_writer.writerow([email, subject, message])
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
